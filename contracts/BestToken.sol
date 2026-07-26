8357321ae2a50ea1f27ee8bdc9cc5bf35edcd36f
Nazi <mehdizadeh.sn@gmail.com>
2026-07-26 08:03:16 +0000

    Add BestToken constructor with parent initialization and initial mint

--- contracts/BestToken.sol
+++ contracts/BestToken.sol
@@ -7,5 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract BestToken is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step{
-    
+   constructor(string memory name_, string memory symbol_, address owner_) ERC20(name_, symbol_) ERC20Permit(name_) Ownable(owner_){
+    _mint(owner_, 10 ** 6 * 10 ** decimals());
+
+   } 
 }
