34a0da18cd53e7d1e18813ecd1ba4901a0a7e46e
Nazi <mehdizadeh.sn@gmail.com>
2026-07-26 06:57:42 +0000

    Import OZ v5 ERC20, ERC20Burnable, ERC20Permit, Ownable2Step and Set up multiple inheritance with valid C3 linearization order

--- contracts/BestToken.sol
+++ contracts/BestToken.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract BestToken is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step{
+    
+}
