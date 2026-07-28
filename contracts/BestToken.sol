2c28bc9985e6aee5122516df9cd54c7713fb11d8
Nazi <mehdizadeh.sn@gmail.com>
2026-07-28 06:58:52 +0000

    import ERC20Capped contract

--- contracts/BestToken.sol
+++ contracts/BestToken.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
@@ -9,6 +10,7 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 contract BestToken is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step{
    constructor(string memory name_, string memory symbol_, address owner_) ERC20(name_, symbol_) ERC20Permit(name_) Ownable(owner_){
     _mint(owner_, 10 ** 6 * 10 ** decimals());
-
    } 
+
+   
 }
