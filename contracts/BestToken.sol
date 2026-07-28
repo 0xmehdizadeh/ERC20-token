06533cb7e2edec701c42648e9e7c2c1d1027f638
Nazi <mehdizadeh.sn@gmail.com>
2026-07-28 08:08:07 +0000

    add ERC20Capped with cap derived from initial mint amount and update the _update function

--- contracts/BestToken.sol
+++ contracts/BestToken.sol
@@ -7,10 +7,14 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract BestToken is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step{
-   constructor(string memory name_, string memory symbol_, address owner_) ERC20(name_, symbol_) ERC20Permit(name_) Ownable(owner_){
-    _mint(owner_, 10 ** 6 * 10 ** decimals());
-   } 
+contract BestToken is ERC20, ERC20Capped, ERC20Burnable, ERC20Permit, Ownable2Step{
+   constructor(string memory name_, string memory symbol_, address owner_, uint256 initial_) ERC20(name_, symbol_) ERC20Capped(2 * initial_ * 10 ** decimals()) ERC20Permit(name_) Ownable(owner_){
+    _mint(owner_, initial_ * 10 ** decimals());
+   
+   }
 
+   function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Capped) {
+      super._update(from, to, value);
+   }
    
 }
