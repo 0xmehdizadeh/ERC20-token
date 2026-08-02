7d75163ab4783e828bdf4cea905e4989d14e5ec9
Nazi <mehdizadeh.sn@gmail.com>
2026-08-02 07:18:01 +0000

    Add block to test token name

--- test/BestToken.ts
+++ test/BestToken.ts
@@ -1,7 +1,6 @@
 import { expect } from "chai";
 import { network } from "hardhat";
 
-
 describe("BestToken", function () {
  let owner: any;
  let user1: any;
@@ -10,12 +9,15 @@ describe("BestToken", function () {
  let BestToken: any;
 
  beforeEach(async function () {
-  ethers = await network.create();
+  ({ethers} = await network.create());
   [owner, user1, user2] = await ethers.getSigners();
   BestToken = await ethers.deployContract("BestToken", ["BestToken", "BST", owner.address, ethers.parseEther("1000000")]);
   await BestToken.waitForDeployment();
  });
 
- 
+ it("Should set the token's name correctly", async function () {
+   const name = await BestToken.name();
+   expect(name).to.equal("BestToken");
+ });
 
 });
