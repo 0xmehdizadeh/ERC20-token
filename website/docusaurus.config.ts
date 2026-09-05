2dc5c1d14fcf4d3da4599aadc2755c54946a5bd5
Nazi <mehdizadeh.sn@gmail.com>
2026-09-05 06:36:12 +0000

    clean up docusaurus config: remove blog, fix branding and links

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -44,23 +44,9 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/0xmehdizadeh/ERC20-token/tree/master/website/",
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
+        blog: false,
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -77,7 +63,7 @@ const config: Config = {
     navbar: {
       title: "BestToken Docs",
       logo: {
-        alt: "My Site Logo",
+        alt: "BestToken Logo",
         src: "img/logo.svg",
       },
       items: [
@@ -87,7 +73,6 @@ const config: Config = {
           position: "left",
           label: "Docs",
         },
-        { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/0xmehdizadeh/ERC20-token",
           label: "GitHub",
@@ -102,25 +87,8 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "X",
-              href: "https://x.com/docusaurus",
+              label: "API Reference",
+              to: "/docs/api/BestToken",
             },
           ],
         },
@@ -128,17 +96,13 @@ const config: Config = {
           title: "More",
           items: [
             {
-              label: "Blog",
-              to: "/blog",
-            },
-            {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/0xmehdizadeh/ERC20-token",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} BestToken. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
